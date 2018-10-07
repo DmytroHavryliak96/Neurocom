e85014017671f63aef1ec197e5cbc8820ec6eab5
dimon <dmytro.havryliak@gmail.com>
2018-10-07 14:07:42 +0300

    start

---
+++ Neurocom/Neurocom/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Neurocom.MvcApplication" Language="C#" %>
---
+++ Neurocom/Neurocom/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Neurocom.Startup))]
+namespace Neurocom
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
