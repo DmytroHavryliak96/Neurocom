0e3ef17ecbb6916f2dfcc80a2788368c4f6a0486
lubomir97 <Lubomir_demchuk@i.ua>
2018-11-28 10:35:31 +0200

    User photo complited

---
+++ Neurocom/Neurocom/BL/Services/CaptchaImage.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Web;
+
+namespace Neurocom.BL.Services
+{
+    public class CaptchaImage
+    {
+        private string text;
+        private int width;
+        private int height;
+        public Bitmap Image { get; set; }
+
+        public CaptchaImage(string s, int width, int height)
+        {
+            text = s;
+            this.width = width;
+            this.height = height;
+            GenerateImage();
+        }
+
+        private void GenerateImage()
+        {
+            Bitmap bitmap = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+
+            Graphics g = Graphics.FromImage(bitmap);
+            g.DrawString(text, new Font("Arial", height / 2, FontStyle.Bold),
+                                        Brushes.Red, new RectangleF(0, 0, width, height));
+            g.Dispose();
+
+            Image = bitmap;
+        }
+        ~CaptchaImage()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+                Image.Dispose();
+        }
+    }
+}
---
+++ Neurocom/Neurocom/Controllers/BranchController.cs
@@ -0,0 +1,69 @@
+﻿using Neurocom.BL.Services;
+using Neurocom.DAO.Interfaces;
+using Neurocom.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Neurocom.Controllers
+{
+    public class BranchController : Controller
+    {
+        private IUnitOfWork db;
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public FileContentResult GetImage(int taskId)
+        {
+            TaskNetwork task = db.TaskNetworks.Get(taskId);
+            if (task != null)
+            {
+                return File(task.ImageData, task.ImageMimeType);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public FileContentResult GetAvatar(int userId)
+        {
+            ApplicationUser user = db.Users.Get(userId);
+            if (user != null)
+            {
+                return File(user.ImageData, user.ImageMimeType);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public FileContentResult GetCaptcha()
+        {
+            string code = new Random(DateTime.Now.Millisecond).Next(1111, 9999).ToString();
+            Session["code"] = code;
+            CaptchaImage captcha = new CaptchaImage(code, 110, 50);
+            return File(ToByteArray(captcha.Image, ImageFormat.Jpeg), "Jpeg");
+
+        }
+
+        public static byte[] ToByteArray(Image image, ImageFormat format)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                image.Save(ms, format);
+                return ms.ToArray();
+            }
+        }
+    }
+}
