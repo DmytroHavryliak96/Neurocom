cb1034297fee8c703351f7406dccd3f59e9d7adb
dimon <dmytro.havryliak@gmail.com>
2018-12-03 21:02:04 +0200

    edit user for admin started

--- Neurocom/Neurocom/ViewModels/AdminViewModels/ApplicationUserViewModel.cs
+++ Neurocom/Neurocom/ViewModels/AdminViewModels/ApplicationUserViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,9 +10,19 @@ namespace Neurocom.ViewModels.AdminViewModels
     public class ApplicationUserViewModel
     {
         public string Id { get; set; }
+
+        [Required]
+        [Display(Name = "І'я користувача")]
         public string UserName { get; set; }
+
+        [EmailAddress]
+        [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Display(Name = "Адреса")]
         public string Address { get; set; }
+
+        [Display(Name = "Номер телефону")]
         public string PhoneNumber { get; set; }
 
         public byte[] ImageData { get; set; }
