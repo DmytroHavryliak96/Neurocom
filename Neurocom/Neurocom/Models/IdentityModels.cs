79272f7cc4a6c4ece629a3f4085ebd7dd82393c0
dimon <dmytro.havryliak@gmail.com>
2019-05-27 13:27:53 +0300

    Revert "Genetic class added"
    This reverts commit 45c2af34fd38c96ac768e5edbdc6fe456622d199.

--- Neurocom/Neurocom/Models/IdentityModels.cs
+++ Neurocom/Neurocom/Models/IdentityModels.cs
@@ -115,7 +115,7 @@ namespace Neurocom.Models
                 const int kerogenAmount = 19;
                 const int layerAmount = 15;
                 const int typesAmount = 2;
-                const int networkAmount = 3;
+                const int networkAmount = 2;
 
                 KerogenRepository dbKerogens = new KerogenRepository(db);
                 Kerogen[] kerogens = new Kerogen[kerogenAmount] {
@@ -189,8 +189,7 @@ namespace Neurocom.Models
                 NeuralNetwork[] networks = new NeuralNetwork[networkAmount]
                 {
                     new NeuralNetwork {Name = "BPN", Description = "param", NetworkTypeId = 1 },
-                    new NeuralNetwork {Name = "LVQ", Description = "param", NetworkTypeId = 2 },
-                    new NeuralNetwork {Name = "GeneticBPN", Description = "param", NetworkTypeId = 1 }
+                    new NeuralNetwork {Name = "LVQ", Description = "param", NetworkTypeId = 2 }
                 };
 
                 for (int i = 0; i < networkAmount; i++)
@@ -205,9 +204,7 @@ namespace Neurocom.Models
                     new AvailableNetwork {NeuralNetworkId = GetNetworkId("BPN", dbNetworks), TaskId =  GetTaskId("Kerogen", dbTasks)},
                     new AvailableNetwork {NeuralNetworkId = GetNetworkId("BPN", dbNetworks), TaskId =  GetTaskId("Layer", dbTasks)},
                     new AvailableNetwork {NeuralNetworkId = GetNetworkId("LVQ", dbNetworks), TaskId =  GetTaskId("Kerogen", dbTasks)},
-                    new AvailableNetwork {NeuralNetworkId = GetNetworkId("LVQ", dbNetworks), TaskId =  GetTaskId("Layer", dbTasks)},
-                    new AvailableNetwork {NeuralNetworkId = GetNetworkId("GeneticBPN", dbNetworks), TaskId =  GetTaskId("Kerogen", dbTasks)},
-                    new AvailableNetwork {NeuralNetworkId = GetNetworkId("GeneticBPN", dbNetworks), TaskId =  GetTaskId("Layer", dbTasks)}
+                    new AvailableNetwork {NeuralNetworkId = GetNetworkId("LVQ", dbNetworks), TaskId =  GetTaskId("Layer", dbTasks)}
                 };
 
                 for (int i = 0; i < aNetworks.Length; i++)
